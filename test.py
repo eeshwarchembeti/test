451d53a0e00a09e5749501cb342a42565f354e7d
eeshwarchembeti <107532843+eeshwarchembeti@users.noreply.github.com>
2024-01-24 15:46:02 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,5 @@
+print("lets see what we can do with this")
+j=int(input("enter the value of countdown"))
+for i in range(5):
+  print(i)
+print("this was build stage where the python code was written")
